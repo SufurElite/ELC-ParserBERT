cf5094840159e324a08e319598816d18fbae638c
“SufurElite” <13058966+SufurElite@users.noreply.github.com>
2024-09-18 14:34:21 -0400

    analysis init

--- analysis/get_diff.py
+++ analysis/get_diff.py
@@ -49,14 +49,29 @@ def get_difference(
     )
     assert len(model_one_results) == len(model_two_results)
 
+    sys.stdout.write(
+        f"Model One: {model_one}\nModel Two: {model_two}\nTask: {task}\nSubtask: {subtask}\n\n"
+    )
+
     model_one_right = []
     model_two_right = []
+    both_wrong = []
+    both_right = []
 
     for idx in range(len(model_one_results)):
         assert model_one_results[idx]["doc_id"] == model_two_results[idx]["doc_id"]
-        if model_one_results[idx]["acc"] == model_two_results[idx]["acc"]:
-            continue
         doc = model_one_results[idx]["doc"]
+        if (
+            model_one_results[idx]["acc"] == model_two_results[idx]["acc"]
+            and model_one_results[idx]["acc"] == 1.0
+        ):
+            both_right.append(doc)
+            continue
+        elif (
+            model_one_results[idx]["acc"] == model_two_results[idx]["acc"]
+            and model_one_results[idx]["acc"] == 0.0
+        ):
+            both_wrong.append(doc)
         if model_one_results[idx]["acc"] == 1.0:
             model_one_right.append(doc)
         elif model_two_results[idx]["acc"] == 1.0:
@@ -73,19 +88,19 @@ def get_difference(
         contextDiff = doc["ContextDiff"]
         targetDiff = doc["TargetDiff"]
         if conceptA not in concepts:
-            concepts[conceptA] = [0, 0, 0]
+            concepts[conceptA] = [0, 0, 0, 0]
         concepts[conceptA][0] += 1
         if conceptB not in concepts:
-            concepts[conceptB] = [0, 0, 0]
+            concepts[conceptB] = [0, 0, 0, 0]
         concepts[conceptB][0] += 1
         if contextType not in contextTypes:
-            contextTypes[contextType] = [0, 0, 0]
+            contextTypes[contextType] = [0, 0, 0, 0]
         contextTypes[contextType][0] += 1
         if contextDiff not in contextDiffs:
-            contextDiffs[contextDiff] = [0, 0, 0]
+            contextDiffs[contextDiff] = [0, 0, 0, 0]
         contextDiffs[contextDiff][0] += 1
         if targetDiff not in targetDiffs:
-            targetDiffs[targetDiff] = [0, 0, 0]
+            targetDiffs[targetDiff] = [0, 0, 0, 0]
         targetDiffs[targetDiff][0] += 1
 
     for doc in model_two_right:
@@ -95,21 +110,65 @@ def get_difference(
         contextDiff = doc["ContextDiff"]
         targetDiff = doc["TargetDiff"]
         if conceptA not in concepts:
-            concepts[conceptA] = [0, 0, 0]
+            concepts[conceptA] = [0, 0, 0, 0]
         concepts[conceptA][1] += 1
         if conceptB not in concepts:
-            concepts[conceptB] = [0, 0, 0]
+            concepts[conceptB] = [0, 0, 0, 0]
         concepts[conceptB][1] += 1
         if contextType not in contextTypes:
-            contextTypes[contextType] = [0, 0, 0]
+            contextTypes[contextType] = [0, 0, 0, 0]
         contextTypes[contextType][1] += 1
         if contextDiff not in contextDiffs:
-            contextDiffs[contextDiff] = [0, 0, 0]
+            contextDiffs[contextDiff] = [0, 0, 0, 0]
         contextDiffs[contextDiff][1] += 1
         if targetDiff not in targetDiffs:
-            targetDiffs[targetDiff] = [0, 0, 0]
+            targetDiffs[targetDiff] = [0, 0, 0, 0]
         targetDiffs[targetDiff][1] += 1
 
+    for doc in both_right:
+        conceptA = doc["ConceptA"]
+        conceptB = doc["ConceptB"]
+        contextType = doc["ContextType"]
+        contextDiff = doc["ContextDiff"]
+        targetDiff = doc["TargetDiff"]
+        if conceptA not in concepts:
+            concepts[conceptA] = [0, 0, 0, 0]
+        concepts[conceptA][2] += 1
+        if conceptB not in concepts:
+            concepts[conceptB] = [0, 0, 0, 0]
+        concepts[conceptB][2] += 1
+        if contextType not in contextTypes:
+            contextTypes[contextType] = [0, 0, 0, 0]
+        contextTypes[contextType][2] += 1
+        if contextDiff not in contextDiffs:
+            contextDiffs[contextDiff] = [0, 0, 0, 0]
+        contextDiffs[contextDiff][2] += 1
+        if targetDiff not in targetDiffs:
+            targetDiffs[targetDiff] = [0, 0, 0, 0]
+        targetDiffs[targetDiff][2] += 1
+
+    for doc in both_wrong:
+        conceptA = doc["ConceptA"]
+        conceptB = doc["ConceptB"]
+        contextType = doc["ContextType"]
+        contextDiff = doc["ContextDiff"]
+        targetDiff = doc["TargetDiff"]
+        if conceptA not in concepts:
+            concepts[conceptA] = [0, 0, 0, 0]
+        concepts[conceptA][3] += 1
+        if conceptB not in concepts:
+            concepts[conceptB] = [0, 0, 0, 0]
+        concepts[conceptB][3] += 1
+        if contextType not in contextTypes:
+            contextTypes[contextType] = [0, 0, 0, 0]
+        contextTypes[contextType][3] += 1
+        if contextDiff not in contextDiffs:
+            contextDiffs[contextDiff] = [0, 0, 0, 0]
+        contextDiffs[contextDiff][3] += 1
+        if targetDiff not in targetDiffs:
+            targetDiffs[targetDiff] = [0, 0, 0, 0]
+        targetDiffs[targetDiff][3] += 1
+    """
     conceptsKeys = set(concepts.keys())
     contextTypesKeys = set(contextTypes.keys())
     contextDiffsKeys = set(contextDiffs.keys())
@@ -131,29 +190,54 @@ def get_difference(
             contextDiffs[contextDiff][2] += 1
         if targetDiff in targetDiffsKeys:
             targetDiffs[targetDiff][2] += 1
-
+    """
+    significant_subtasks = []
     sys.stdout.write(f"{'='*120}\n\n")
     sys.stdout.write("Concepts:\n")
     sys.stdout.write(
-        f"{'Concept Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Total Instances':20} | {'Percentage of Total':25}\n"
+        f"{'Concept Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Both Right':12} | {'Both Wrong':12} | {'Percentage of Total':25} | {'p-value':8}\n"
     )
     sys.stdout.write(f"{'-'*120}\n")
 
     for concept in concepts:
         percentage = round(
-            ((concepts[concept][0] + concepts[concept][1]) / concepts[concept][2]),
+            (
+                (concepts[concept][0] + concepts[concept][1])
+                / (
+                    concepts[concept][0]
+                    + concepts[concept][1]
+                    + concepts[concept][2]
+                    + concepts[concept][3]
+                )
+            ),
             ndigits=2,
         )
-        sys.stdout.write(
-            f"{concept:15} | {concepts[concept][0]:20} | {concepts[concept][1]:20} | {concepts[concept][2]:20} | {percentage:25}\n"
+        """
+        2x2 array where 
+        correct/correct, correct/incorrect
+        incorrect/correct, incorrect/incorrect
+        """
+        mcnemar_table = np.array(
+            [
+                [concepts[concept][2], concepts[concept][0]],
+                [concepts[concept][1], concepts[concept][3]],
+            ]
         )
+
+        _, p_value = mcnemar(ary=mcnemar_table, corrected=True)
+
+        sys.stdout.write(
+            f"{concept:15} | {concepts[concept][0]:20} | {concepts[concept][1]:20} | {concepts[concept][2]:12} | {concepts[concept][3]:12} | {percentage:25} | {p_value:8}\n"
+        )
+        if p_value < 0.05:
+            significant_subtasks.append((concept, p_value))
     sys.stdout.write("\n")
     sys.stdout.write(f"{'='*120}\n\n")
 
     sys.stdout.write("\n")
     sys.stdout.write("Context Types:\n")
     sys.stdout.write(
-        f"{'Context Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Total Instances':20} | {'Percentage of Total':25}\n"
+        f"{'Context Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Both Right':12} | {'Both Wrong':12} | {'Percentage of Total':25} | {'p-value':8}\n"
     )
     sys.stdout.write(f"{'-'*120}\n")
 
@@ -161,20 +245,37 @@ def get_difference(
         percentage = round(
             (
                 (contextTypes[context][0] + contextTypes[context][1])
-                / contextTypes[context][2]
+                / (
+                    contextTypes[context][0]
+                    + contextTypes[context][1]
+                    + contextTypes[context][2]
+                    + contextTypes[context][3]
+                )
             ),
             ndigits=2,
         )
-        sys.stdout.write(
-            f"{context:15} | {contextTypes[context][0]:20} | {contextTypes[context][1]:20} | {contextTypes[context][2]:20} | {percentage:25}\n"
+        mcnemar_table = np.array(
+            [
+                [contextTypes[context][2], contextTypes[context][0]],
+                [contextTypes[context][1], contextTypes[context][3]],
+            ]
         )
+
+        _, p_value = mcnemar(ary=mcnemar_table, corrected=True)
+
+        sys.stdout.write(
+            f"{context:15} | {contextTypes[context][0]:20} | {contextTypes[context][1]:20} | {contextTypes[context][2]:12} |  {contextTypes[context][3]:12} | {percentage:25} | {p_value:8}\n"
+        )
+        if p_value < 0.05:
+            significant_subtasks.append((context, p_value))
+
     sys.stdout.write("\n")
     sys.stdout.write(f"{'='*120}\n\n")
 
     sys.stdout.write("\n")
     sys.stdout.write("Context Diffs:\n")
     sys.stdout.write(
-        f"{'Context Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Total Instances':20} | {'Percentage of Total':25}\n"
+        f"{'Context Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Both Right':12} | {'Both Wrong':12} | {'Percentage of Total':25} | {'p-value':8}\n"
     )
     sys.stdout.write(f"{'-'*120}\n")
 
@@ -182,34 +283,73 @@ def get_difference(
         percentage = round(
             (
                 (contextDiffs[context][0] + contextDiffs[context][1])
-                / contextDiffs[context][2]
+                / (
+                    contextDiffs[context][0]
+                    + contextDiffs[context][1]
+                    + contextDiffs[context][2]
+                    + contextDiffs[context][3]
+                )
             ),
             ndigits=2,
         )
-        sys.stdout.write(
-            f"{context:15} | {contextDiffs[context][0]:20} | {contextDiffs[context][1]:20} | {contextDiffs[context][2]:20} | {percentage:25}\n"
+        mcnemar_table = np.array(
+            [
+                [contextDiffs[context][2], contextDiffs[context][0]],
+                [contextDiffs[context][1], contextDiffs[context][3]],
+            ]
         )
+
+        _, p_value = mcnemar(ary=mcnemar_table, corrected=True)
+
+        sys.stdout.write(
+            f"{context:15} | {contextDiffs[context][0]:20} | {contextDiffs[context][1]:20} | {contextDiffs[context][2]:12} |  {contextDiffs[context][3]:12} | {percentage:25} | {p_value:8}\n"
+        )
+        if p_value < 0.05:
+            significant_subtasks.append((context, p_value))
+
     sys.stdout.write("\n")
     sys.stdout.write(f"{'='*120}\n\n")
 
     sys.stdout.write("Target Diffs:\n")
     sys.stdout.write(
-        f"{'Target Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Total Instances':20} | {'Percentage of Total':25}\n"
+        f"{'Target Name':15} | {'Model One Correct':20} | {'Model Two Correct':20} | {'Both Right':12} | {'Both Wrong':12} | {'Percentage of Total':25} | {'p-value':8}\n"
     )
     sys.stdout.write(f"{'-'*120}\n")
     for target in targetDiffs:
         percentage = round(
             (
                 (targetDiffs[target][0] + targetDiffs[target][1])
-                / targetDiffs[target][2]
+                / (
+                    targetDiffs[target][0]
+                    + targetDiffs[target][1]
+                    + targetDiffs[target][2]
+                    + targetDiffs[target][3]
+                )
             ),
             ndigits=2,
         )
-        sys.stdout.write(
-            f"{target:15} | {targetDiffs[target][0]:20} | {targetDiffs[target][1]:20} | {targetDiffs[target][2]:20} | {percentage:25}\n"
+
+        mcnemar_table = np.array(
+            [
+                [targetDiffs[target][2], targetDiffs[target][0]],
+                [targetDiffs[target][1], targetDiffs[target][3]],
+            ]
         )
+
+        _, p_value = mcnemar(ary=mcnemar_table, corrected=True)
+
+        sys.stdout.write(
+            f"{target:15} | {targetDiffs[target][0]:20} | {targetDiffs[target][1]:20} | {targetDiffs[target][2]:12} | {targetDiffs[target][3]:12} | {percentage:25} | {p_value:8}\n"
+        )
+        if p_value < 0.05:
+            significant_subtasks.append((target, p_value))
+
     sys.stdout.write("\n")
-    sys.stdout.write(f"{'='*120}\n")
+    sys.stdout.write(f"{'='*120}\n\n")
+    sys.stdout.write(f"Significant Categories:\n")
+    significant_subtasks = sorted(significant_subtasks, key=itemgetter(1), reverse=True)
+    for values in significant_subtasks:
+        sys.stdout.write(f"{values[0]} {values[1]}\n")
 
 
 def calculate_mcnemar(model_one_path, model_two_path, debug: bool = True):
--- analysis/tree_functions.py
+++ analysis/tree_functions.py
@@ -1,4 +1,220 @@
 """
 Tree functions helper implemented based on the algorithms from the StructFormer paper: https://arxiv.org/pdf/2012.00857
 """
+from nltk.tree import Tree
+from typing import List
 
+
+def build_tree_whole_words(
+    subwords: List,
+    distances: List,
+    heights: List,
+    space_char: str = "Ġ",
+    debug: bool = False,
+):
+    """
+    params:
+        subwords
+        distances
+        heights
+        debug
+
+    before running build tree, given that the model creates subwords, we
+    recombine the subwords into their original words and average
+    the heights/distances across the subwords
+    """
+
+    assert len(subwords) == len(distances) and len(distances) == len(heights)
+    words = []
+    new_heights = []
+    new_distances = []
+    last_idx = 0
+    if subwords[0][0] == space_char:
+        subwords[0] = subwords[0][1:]
+    for idx in range(len(subwords)):
+        new_idx = 0
+        if subwords[idx][0] == space_char:
+            new_idx = idx
+        else:
+            continue
+        tmp_height = 0
+        tmp_distance = 0
+        for i in range(last_idx, new_idx):
+            subwords[i] = subwords[i].replace(space_char, "")
+            tmp_height += heights[i]
+            tmp_distance += distances[i]
+        tmp_height = tmp_height / (new_idx - last_idx)
+        tmp_distance = tmp_height / (new_idx - last_idx)
+        new_heights.append(tmp_height)
+        new_distances.append(tmp_distance)
+        words = words + ["".join(subwords[last_idx:new_idx])]
+        last_idx = new_idx
+
+    tmp_height = 0
+    tmp_distance = 0
+    for i in range(last_idx, len(subwords)):
+        subwords[i] = subwords[i].replace(space_char, "")
+        tmp_height += heights[i]
+        tmp_distance += distances[i]
+    tmp_height = tmp_height / (len(subwords) - last_idx)
+    tmp_distance = tmp_distance / (len(subwords) - last_idx)
+    new_heights.append(tmp_height)
+    new_distances.append(tmp_distance)
+
+    words = words + ["".join(subwords[last_idx:])]
+
+    assert len(new_distances) == len(words) and len(words) == len(new_heights)
+
+    return build_tree(words, new_distances, new_heights, debug)
+
+
+def build_tree(words: List, distances: List, heights: List, debug: bool = False):
+    """
+    params:
+        words
+        distances
+        heights
+        debug
+    """
+    t = None
+    dependency_graph = []
+    parent = None
+    height = None
+    if debug:
+        print("distances: ", distances)
+        print("heights: ", heights)
+        print("words: ", words)
+        print("\n\n")
+
+    if len(distances) == 0 and len(words) == 1 and len(heights) == 1:
+        t = Tree(words[0], [])
+        dependency_graph = []
+        parent = words[0]
+        height = heights[0]
+    else:
+        # get the index of the next largest distance
+        max_value = max(distances)
+        max_index = distances.index(max_value)
+        Tree_left, dependency_left, parent_left, height_left = build_tree(
+            words[: max_index + 1], distances[:max_index], heights[: max_index + 1]
+        )
+        if len(words[max_index + 1 :]) == 0:
+            return Tree_left, dependency_left, parent_left, height_left
+        Tree_right, dependency_right, parent_right, height_right = build_tree(
+            words[max_index + 1 :], distances[max_index + 1 :], heights[max_index + 1 :]
+        )
+        dependency_graph = dependency_left + dependency_right
+        t = Tree("*", [Tree_left, Tree_right])
+        if height_left > height_right:
+            height = height_left
+            parent = parent_left
+            dependency_graph.append([parent_left, parent_right])
+        else:
+            parent = parent_right
+            height = height_right
+            dependency_graph.append([parent_right, parent_left])
+
+    return t, dependency_graph, parent, height
+
+
+def model_to_tree_output(input: str, tokenizer, model):
+    space_char = tokenizer.convert_ids_to_tokens(tokenizer(" ").input_ids)[0]
+    subwords = tokenizer.convert_ids_to_tokens(tokenizer(input).input_ids)
+    tree_values, _ = model(tokenizer.encode(input, return_tensors="pt"))
+    heights = tree_values["height"]
+    distances = tree_values["distance"]
+    tree, _, _, _ = build_tree_whole_words(
+        subwords, distances, heights, space_char=space_char
+    )
+    tree.pretty_print()
+    return tree
+
+
+if __name__ == "__main__":
+    subwords = [
+        "ĠThe",
+        "ĠBaby",
+        "L",
+        "M",
+        "ĠCha",
+        "llen",
+        "ge",
+        "Ġwill",
+        "Ġbe",
+        "Ġheld",
+        "Ġagain",
+        "Ġas",
+        "Ġthe",
+        "Ġshared",
+        "Ġtask",
+        "Ġfor",
+        "ĠCo",
+        "N",
+        "LL",
+        "Ġ",
+        "2",
+        "0",
+        "2",
+        "4",
+    ]
+
+    distances = [
+        -2.5962e-01,
+        -6.7681e-01,
+        -7.1163e-02,
+        -9.5030e-01,
+        -4.0385e-01,
+        -9.4605e-01,
+        -1.1227e00,
+        -6.9064e-01,
+        -1.1060e00,
+        -1.0958e00,
+        -8.7155e-01,
+        -6.3248e-01,
+        -9.0634e-01,
+        -8.5295e-01,
+        -6.4920e-01,
+        -8.4231e-01,
+        -8.9462e-01,
+        -6.8456e-01,
+        -3.9250e-01,
+        -7.9879e-01,
+        -7.1378e-01,
+        -9.0493e-02,
+        -4.1572e-01,
+        9.9993e03,
+    ]
+
+    heights = [
+        0.4022,
+        0.1320,
+        -0.5007,
+        0.2278,
+        -0.0570,
+        0.1832,
+        -0.8603,
+        0.0759,
+        0.3247,
+        0.2826,
+        0.2969,
+        0.1764,
+        -0.5244,
+        -0.4788,
+        -0.0916,
+        0.6123,
+        -0.3484,
+        -0.1902,
+        -0.1880,
+        -0.6134,
+        0.2112,
+        -0.0740,
+        -0.0905,
+        -0.1619,
+    ]
+
+    assert len(subwords) == len(distances) and len(heights) == len(distances)
+
+    t, dependency_graph, parent, height = build_tree_whole_words(
+        subwords, distances, heights
+    )
+    t.pretty_print()
